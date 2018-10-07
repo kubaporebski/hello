60c5a0c87ad9f8aaa27bbb8dd3e8c330f95ce951
kubaporebski <jakub-porebski@outlook.com>
2018-10-07 08:45:50 +0100

    Dodanie jakiegoś pliczku.

---
+++ DatabaseProjektTest/DatabaseProjektTest/Script1.sql
@@ -0,0 +1,3 @@
+﻿select 1 as X
+go
+
--- DatabaseProjektTest/DatabaseProjektTest/Table1.sql
+++ DatabaseProjektTest/DatabaseProjektTest/Table1.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Table1]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [StringValue] VARCHAR(1024) NOT NULL
+    [StringValue] VARCHAR(1024) NULL, 
+    [IntValue] INT NULL
 )
